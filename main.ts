d5f17b2314e867458adf9f563e9c3f5e0d7831dd
Andrew <afmcdnl@gmail.com>
2020-08-06 23:27:45 -0400

    Fixed the signals back and forth.

--- main.ts
+++ main.ts
@@ -7,6 +7,23 @@ radio.onReceivedNumber(function (receivedNumber) {
     // If the first micro:bit sends a temperaute other 37 degrees - send a message asking if they need help. 
     if (receivedNumber > 37) {
         radio.sendString("help?")
+        help_state = 1
+    }
+    // Display the tmperauter 
+    basic.showNumber(receivedNumber)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    if (receivedString == "Cancel help") {
+        help_state = 0
+    }
+})
+let help_state = 0
+radio.setGroup(1)
+help_state = 0
+basic.forever(function () {
+    if (help_state == 1) {
         music.playTone(131, music.beat(BeatFraction.Sixteenth))
         music.playTone(147, music.beat(BeatFraction.Sixteenth))
         music.playTone(165, music.beat(BeatFraction.Sixteenth))
@@ -25,12 +42,6 @@ radio.onReceivedNumber(function (receivedNumber) {
         music.playTone(698, music.beat(BeatFraction.Sixteenth))
         music.playTone(880, music.beat(BeatFraction.Sixteenth))
         music.playTone(988, music.beat(BeatFraction.Sixteenth))
-        basic.pause(500)
+        basic.pause(1000)
     }
-    // Display the tmperauter 
-    basic.showNumber(receivedNumber)
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString("Hello!")
-})
-radio.setGroup(1)
